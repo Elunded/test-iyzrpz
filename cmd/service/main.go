d92ab9bccd0266e7f8665f6c83634764cf3ae850
Elunded <demonschenel@gmail.com>
2026-06-04 13:34:53 +0200

    fix: address review comments

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -11,6 +11,5 @@ func main() {
 	logger := zerolog.New(os.Stdout).With().Timestamp().Str("service", "image-metadata-processor").Logger()
 
 	logger.Info().Msg("Image Metadata Processor started...")
-
 	time.Sleep(1 * time.Hour)
 }
